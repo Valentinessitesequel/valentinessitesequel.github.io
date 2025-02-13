0ba2e5ae20e01d4f5036457ed17c7186079312a6
Caleb Henry <calebhenry2003+2@gmail.com>
2025-02-12 21:05:34 -0500

    Updated to parity with old website

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,10 +1,65 @@
+import { useEffect, useState } from "react";
 import quotes from "../data/quotes.json";
+import "./Note.css";
+import blue from "../res/blue.jpg";
+import orange from "../res/orange.jpg";
+import red from "../res/red.jpg";
+import pink from "../res/pink.jpg";
+import green from "../res/green.jpg";
+import purple from "../res/purple.jpg";
+import cork from "../res/cork.jpg";
+import chalk from "../res/chalk.jpg";
 
-const Note = () => {
-	const quote = quotes[Math.floor(Math.random() * quotes.length)];
+const Note = ({ index }) => {
+	const quote = quotes[index];
+	const [note, setNote] = useState(blue);
+
+	useEffect(() => {
+		const day = new Date().getDate();
+
+		let numColors = 4;
+		if (day < 14) {
+			numColors = 8;
+		} else if (day > 26) {
+			numColors = 6;
+		}
+
+		const paperType = Math.floor(Math.random() * numColors);
+
+		switch (paperType) {
+			case 0:
+				setNote(blue);
+				break;
+			case 1:
+				setNote(orange);
+				break;
+			case 2:
+				setNote(red);
+				break;
+			case 3:
+				setNote(pink);
+				break;
+			case 4:
+				setNote(green);
+				break;
+			case 5:
+				setNote(purple);
+				break;
+			case 6:
+				setNote(cork);
+				break;
+			default:
+				setNote(chalk);
+				break;
+		}
+	}, [quote]);
+
 	return (
-		<div className="note">
-			<p>{quote.quote}</p>
+		<div
+			className="quote-container"
+			style={{ backgroundImage: `url(${note})` }}
+		>
+			<p className="quote">{quote.quote}</p>
 		</div>
 	);
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,74 @@
 import { useEffect, useState } from "react";
-import quotes from "../data/quotes.json";
 import "./Home.css";
 import Note from "../components/Note";
+import jar4 from "../res/jar4.PNG";
 import jar1 from "../res/jar1.PNG";
+import jar2 from "../res/jar2.PNG";
+import jar3 from "../res/jar3.PNG";
 
-const Home = () => {
-	console.log(quotes);
-
+const Home = ({ toggleMode }) => {
 	const [opened, setOpened] = useState(false);
-	const [year, setYear] = useState(2024);
+	const [jar, setJar] = useState(jar4);
+	const [noteIndex, setNoteIndex] = useState(0);
+
+	useEffect(() => {
+		//Get the current date
+		const day = new Date().getDate();
+		let month = new Date().getMonth();
+		let year = new Date().getFullYear();
+
+		if (day < 14) {
+			if (month === 0) {
+				month = 11;
+				year = year - 1;
+			} else {
+				month = month - 1; // If it is not the yet the 14th, prevent the month from advancing
+			}
+		}
+
+		//Calculate the number of days since the last 14th
+		const valDate = new Date(year, month, 14).getTime();
+		const currDate = new Date().getTime();
+		const daysSinceValDay = Math.round(
+			(currDate - valDate) / (1000 * 3600 * 24)
+		);
+
+		console.log(daysSinceValDay);
+
+		const jarType = Math.floor(daysSinceValDay / 9);
+
+		console.log(jarType);
+
+		switch (jarType) {
+			case 0:
+				setJar(jar1);
+				break;
+			case 1:
+				setJar(jar2);
+				break;
+			case 2:
+				setJar(jar3);
+				break;
+			default:
+				setJar(jar4);
+				break;
+		}
+
+		setNoteIndex(day);
+	}, []);
 
 	return (
 		<div className="container">
 			{opened ? (
-				<Note />
+				<Note index={noteIndex} />
 			) : (
-				<button style={{ border: "none", background: "none" }}>
+				<button className="jar-container">
 					<img
-						src={jar1}
+						src={jar}
 						onClick={() => setOpened(!opened)}
 						alt="jar"
-						style={{ width: "40%" }}
-					></img>
+						className="jar"
+					/>
 				</button>
 			)}
 		</div>
